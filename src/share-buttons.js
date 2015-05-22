63cb882cfb750d1091eaad0f01492f9d628ebab3
Evgeniy Pakalo <evgeniy.pakalo@gmail.com>
2015-05-22 16:50:19 +0300

    Refactoring script

--- src/share-buttons.js
+++ src/share-buttons.js
@@ -1,170 +1,182 @@
-(function () {
+(function (window, document) {
 	'use strict';
 
-	var FB_LINK = 'https://www.facebook.com/sharer/sharer.php?u=';
-	var VK_LINK = 'https://vk.com/share.php?url={0}&description={1}. {2}';
-	var TW_LINK = 'https://twitter.com/intent/tweet?url=';
-	//var GP_LINK = 'https://plus.google.com/share?url=';
-	//var IN_LINK = 'https://www.linkedin.com/shareArticle?mini=true&url=';
-
-	// from http://evgeniy.pakalo.name/post/49
-	var _F = function(str, args){
-		return str.replace(/\{(\d+)\}/g, function(m,n){
-			return args[n] ? args[n] : m;
-		});
-	};
-
+	/**
+	 * Class to display the buttons of social networks.
+	 *
+	 * @author Yauheni Pakala <evgeniy.pakalo@gmail.com>
+	 * @version 1.0
+	 * @license MIT
+	 */
 	function ShareButtons() {
-		var width = 600,
-			height = 400;
+		
+		var FB_LINK_FORMAT = 'https://www.facebook.com/sharer/sharer.php?u=',
+			VK_LINK_FORMAT = 'https://vk.com/share.php?url={0}&description={1}. {2}',
+			TW_LINK_FORMAT = 'https://twitter.com/intent/tweet?url=',
+			MAIL_LINK_FORMAT = 'mailto:?Subject={0}{1}&body={2}{3}',
 
-		this.init = function() {
-			var _this = this,
+			// from http://evgeniy.pakalo.name/post/49
+			stringFormat = function (str, args) {
+				return str.replace(/\{(\d+)\}/g, function (m, n) {
+					return args[n] || m;
+				});
+			};
+
+		/**
+		 * Method for initialize class for all elements
+		 */
+		this.init = function () {
+			var i,
 				share = document.querySelectorAll('.share-btn');
-			
-			for (var i = 0, l = share.length; i < l; i++) {
-				var url = this.getUrl(share[i]),
-					title = this.getTitle(share[i]),
-					desc = this.getDesc(share[i]),
-					el = share[i].querySelectorAll('a');
 
-				for (var a = 0, al = el.length; a < al; a++) {
-					var id = el[a].getAttribute('data-id');
-					if (id) {
-						_this.addEventListener(el[a], 'click', {
-							id: id,
-							url: url,
-							title: title,
-							desc: desc
-						});
-					}
+			for (i in share) {
+				if (share.hasOwnProperty(i)) {
+					this.initForElement(share[i]);
 				}
 			}
 		};
 
+		/**
+		 * Method for initialize class for all elements
+		 */
+		this.initForElement = function (el) {
+			var i,
+				a = el.querySelectorAll('a');
+
+			for (i in a) {
+				if (a.hasOwnProperty(i)) {
+					this.prepareLink(a[i], {
+						id: '',
+						url: this.getUrl(el),
+						title: this.getTitle(el),
+						desc: this.getDesc(el)
+					});
+				}
+			}
+		};
+		
+		/**
+		 * Method for handling click event to link
+		 */
+		this.prepareLink = function (el, options) {
+			var that = this;
+
+			options.id = el.getAttribute('data-id');
+			if (options.id) {
+				that.addEventListener(el, 'click', options);
+			}
+		};
+
+		/**
+		 * Method for getting url from page or options
+		 */
 		this.getUrl = function (share) {
 			return share.getAttribute('data-url') || location.href || ' ';
 		};
 
+		/**
+		 * Method for getting title from page or options
+		 */
 		this.getTitle = function (share) {
 			return share.getAttribute('data-title') || document.title || ' ';
 		};
 
+		/**
+		 * Method for getting description from page or options
+		 */
 		this.getDesc = function (share) {
 			var metaDesc = document.querySelector('meta[name=description]');
 			return share.getAttribute('data-desc') || (metaDesc && metaDesc.getAttribute('content')) || ' ';
 		};
 
-		this.addEventListener = function(el, eventName, opt) {
-			var _this = this,
+		/**
+		 * Method for attaching event to the element
+		 */
+		this.addEventListener = function (el, eventName, opt) {
+			var that = this,
 				handler = function () {
-					_this.share(opt.id, opt.url, opt.title, opt.desc);
+					that.share(opt.id, opt.url, opt.title, opt.desc);
 				};
 
 			if (el.addEventListener) {
 				el.addEventListener(eventName, handler);
 			} else {
-				el.attachEvent('on' + eventName, function() {
+				el.attachEvent('on' + eventName, function () {
 					handler.call(el);
 				});
 			}
 		};
 
-		this.share = function(id, url, title, desc) {
-			url = encodeURIComponent(url);
-			desc = encodeURIComponent(desc);
-			title = encodeURIComponent(title);
+		/**
+		 * Method for handling chosen links
+		 */
+		this.share = function (id, urlDef, titleDef, descDef) {
+			var url = encodeURIComponent(urlDef),
+				desc = encodeURIComponent(descDef),
+				title = encodeURIComponent(titleDef),
+				text = title || desc || '';
 
 			switch (id) {
-				case 'fb':
-					this.popupCenter(FB_LINK + url, this.title, this.width, this.height);
-					break;
+			case 'fb':
+				this.popupCenter(FB_LINK_FORMAT + url, titleDef);
+				break;
 
-				case 'vk':
-					var url = _F(VK_LINK, [url, title, desc]);
+			case 'vk':
+				this.popupCenter(stringFormat(VK_LINK_FORMAT, [url, title, desc]), titleDef);
+				break;
 
-					this.popupCenter(url, this.title, this.width, this.height);
-					break;
+			case 'tw':
+				if (title.length > 0 && desc.length > 0) {
+					text = title + ' - ' + desc;
+				}
+				if (text.length > 0) {
+					text = '&text=' + text;
+				}
 
-				case 'tw':
-					var text = title || desc || '';
-					if (title.length > 0 && desc.length > 0) {
-						text = title + ' - ' + desc;
-					}
-					if (text.length > 0) {
-						text = '&text=' + text;
-					}
-						
-					this.popupCenter(TW_LINK + url + text, this.title, this.width, this.height);
-					break;
+				this.popupCenter(TW_LINK_FORMAT + url + text, titleDef);
+				break;
 
-				// case 'gp':
-				// 	this.popupCenter(GP_LINK + url, this.title, this.width, this.height);
-				// 	break;
+			case 'mail':
+				if (title.length > 0 && desc.length > 0) {
+					text = title + ' - ' + desc;
+				}
+				if (text.length > 0) {
+					text = text + ' / ';
+				}
+				if (title.length > 0) {
+					title += ' / ';
+				}
 
-				// case 'in':
-				// 	this.popupCenter(IN_LINK + url, this.title, this.width, this.height);
-				// 	break;
+				location.href = stringFormat(MAIL_LINK_FORMAT, [title, titleDef, text, url]);
+				break;
 
-				case 'mail':
-					var text = title || desc || '';
-					if (title.length > 0 && desc.length > 0) {
-						text = title + ' - ' + desc;
-					}
-					if (text.length > 0) {
-						text = text + ' / ';
-					}
-					if (title.length > 0) {
-						title = title + ' / ';
-					}
-					
-					var mail = 'mailto:?Subject=' + title + this.title + '&body=' + text + url;
-					this.newTab(mail);
-					break;
-
-				default:
-					break;
-			};
+			default:
+				break;
+			}
 		};
 
-		this.newTab = function (url) {
-			var win = window.open(url, '_blank');
-			win.focus();		
-		};
+		/**
+		 * Method for opening popup window
+		 */
+		this.popupCenter = function (url, title) {
+			var w = 600,
+				h = 400,
+				dualScreenLeft = typeof window.screenLeft !== 'undefined' ? window.screenLeft : screen.left,
+				dualScreenTop = typeof window.screenTop !== 'undefined' ? window.screenTop : screen.top,
+				width = window.innerWidth || document.documentElement.clientWidth || screen.width,
+				height = window.innerHeight || document.documentElement.clientHeight || screen.height,
+				left = ((width / 2) - (w / 2)) + dualScreenLeft,
+				top = ((height / 3) - (h / 3)) + dualScreenTop,
+				windowFormat = 'resizable,toolbar=yes,location=yes,scrollbars=yes,menubar=yes,width={0},height={1},top={2},left={3}',
+				newWindow = window.open(url, '', stringFormat(windowFormat, [w, h, top, left]));
 
-		this.popupCenter = function (url, title, w, h) {
-			var dualScreenLeft = window.screenLeft !== undefined
-				? window.screenLeft
-				: screen.left;
-			var dualScreenTop = window.screenTop !== undefined
-				? window.screenTop
-				: screen.top;
-
-			var width = window.innerWidth
-				? window.innerWidth
-				: document.documentElement.clientWidth
-					? document.documentElement.clientWidth
-					: screen.width;
-			var height = window.innerHeight
-				? window.innerHeight
-				: document.documentElement.clientHeight
-					? document.documentElement.clientHeight
-					: screen.height;
-			
-			var left = ((width / 2) - (w / 2)) + dualScreenLeft;
-			var top = ((height / 3) - (h / 3)) + dualScreenTop;
-
-			var newWindow = window.open(url, title, 'scrollbars=yes, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);
-
-			if (window.focus) {
+			if (newWindow !== null && newWindow.focus) {
 				newWindow.focus();
 			}
-		}
-	};
+		};
+	}
 
-
-	// start	
+	// start
 	new ShareButtons().init();
-}());
 
-	
+}(window, document));
