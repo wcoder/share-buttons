67e18b0d1cfc146c95a6a8390ad46747ffe717bf
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2018-06-01 18:59:44 +0300

    Refactoring

--- src/share-buttons.js
+++ src/share-buttons.js
@@ -10,11 +10,16 @@
      */
     function ShareButtons() {
 
-        var FB_LINK_FORMAT = 'https://www.facebook.com/sharer/sharer.php?u=',
-            VK_LINK_FORMAT = 'https://vk.com/share.php?url={0}&description={1}. {2}',
-            TW_LINK_FORMAT = 'https://twitter.com/intent/tweet?url=',
-            TG_LINK_FORMAT = 'https://telegram.me/share/url?url={0}&text={1}',
+        var FB_LINK_FORMAT = 'https://www.facebook.com/sharer/sharer.php?u={0}',
+            VK_LINK_FORMAT = 'https://vk.com/share.php?url={0}&title={1}',
+            TW_LINK_FORMAT = 'https://twitter.com/intent/tweet?url={0}&text={1}',
+            TG_LINK_FORMAT = 'https://t.me/share/url?url={0}&text={1}',
             MAIL_LINK_FORMAT = 'mailto:?Subject={0}{1}&body={2}{3}',
+            FB_CLASS_NAME = 'fb',
+            VK_CLASS_NAME = 'vk',
+            TW_CLASS_NAME = 'tw',
+            TG_CLASS_NAME = 'tg',
+            MAIL_CLASS_NAME = 'mail',
 
             // from https://wcoder.github.io/notes/string-format-for-string-formating-in-javascript
             stringFormat = function (str, args) {
@@ -116,26 +121,31 @@
                 text = title || desc || '';
 
             switch (id) {
-            case 'fb':
-                this.popupCenter(FB_LINK_FORMAT + url, titleDef);
+            case FB_CLASS_NAME:
+                this.popupCenter(
+                    stringFormat(FB_LINK_FORMAT, [url]),
+                    titleDef);
                 break;
 
-            case 'vk':
-                this.popupCenter(stringFormat(VK_LINK_FORMAT, [url, title, desc]), titleDef);
+            case VK_CLASS_NAME:
+                this.popupCenter(
+                    stringFormat(VK_LINK_FORMAT, [
+                        url,
+                        mergeForTitle([title, desc])
+                    ]),
+                    titleDef);
                 break;
 
-            case 'tw':
-                if (title.length > 0 && desc.length > 0) {
-                    text = title + ' - ' + desc;
-                }
-                if (text.length > 0) {
-                    text = '&text=' + text;
-                }
-
-                this.popupCenter(TW_LINK_FORMAT + url + text, titleDef);
+            case TW_CLASS_NAME:
+                this.popupCenter(
+                    stringFormat(TW_LINK_FORMAT, [
+                        url,
+                        mergeForTitle([title, desc])
+                    ]),
+                    titleDef);
                 break;
 
-            case 'tg':
+            case TG_CLASS_NAME:
                 this.popupCenter(
                     stringFormat(TG_LINK_FORMAT, [
                         url,
@@ -144,9 +154,9 @@
                     titleDef);
                 break;
 
-            case 'mail':
+            case MAIL_CLASS_NAME:
                 if (title.length > 0 && desc.length > 0) {
-                    text = title + ' - ' + desc;
+                    text = mergeForTitle([title, desc]);
                 }
                 if (text.length > 0) {
                     text = text + ' / ';
